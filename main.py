acd327066f7fb936531b6af3f1944951007ecd6d
raythurman2386 <raymondthurman5@gmail.com>
2023-12-19 11:28:37 -0600

    streaming and saving response

--- main.py
+++ main.py
@@ -1,83 +1,117 @@
 from tkinter import scrolledtext, END
 import customtkinter as ctk
 
-from db.database import init_db
+from db.database import add_message, init_db
 import threading
 
 from utils.openai import ask_gpt
 
-
-def handle_response(response):
-    text_area.insert(END, response)
-    text_area.see(END)  # scroll to end
+import customtkinter as ctk
+from tkinter import scrolledtext
 
 
-# Function to handle the 'Generate' button click
-def on_generate_click():
-    prompt = prompt_entry.get()
-    model = model_dropdown.get()
-    temperature = temperature_slider.get()
-    prompt_entry.delete(0, "end")
+class TextGeneratorUI(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
-    def do_generate():
-        response = ask_gpt(prompt, model, temperature)
-        
-        for chunk in response:
-            if chunk.choices[0].delta.content is not None:
-                threading.Thread(target=handle_response, args=(chunk.choices[0].delta.content,)).start()
+        self.title("AI Text Generator")
+        self.geometry("600x625")
+        self.db = init_db()
+        self.full_response = ""
 
-    threading.Thread(target=do_generate).start()
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Fonts
+        default_font = ctk.CTkFont(family="Roboto", size=12)
+        text_area_font = ctk.CTkFont(family="Roboto", size=14)
+
+        # Input frame
+        input_frame = ctk.CTkFrame(self)
+        input_frame.pack(pady=20, padx=20, fill="x")
+
+        # Prompt label
+        prompt_label = ctk.CTkLabel(
+            input_frame, text="Enter Prompt:", font=default_font
+        )
+        prompt_label.pack(side="left", padx=10)
+
+        # Prompt entry
+        self.prompt_entry = ctk.CTkEntry(input_frame, width=200)
+        self.prompt_entry.pack(side="left", expand=True, fill="x")
+
+        # Generate button
+        generate_button = ctk.CTkButton(
+            input_frame, text="Generate", command=self.generate_text, font=default_font
+        )
+        generate_button.pack(side="left", padx=10)
+
+        # Text area
+        self.text_area = scrolledtext.ScrolledText(
+            self, wrap="word", font=text_area_font
+        )
+        self.text_area.pack(pady=10, padx=20, expand=True, fill="both")
+
+        # Settings frame
+        settings_frame = ctk.CTkFrame(self)
+        settings_frame.pack(pady=20, padx=20, fill="x")
+
+        # Model dropdown
+        model_label = ctk.CTkLabel(settings_frame, text="Model:", font=default_font)
+        model_label.pack(side="left", padx=10)
+
+        self.model_dropdown = ctk.CTkComboBox(
+            settings_frame,
+            values=["GPT 3.5 Turbo", "GPT 4", "GPT 4 Turbo"],
+            font=default_font,
+        )
+        self.model_dropdown.set("GPT 3.5 Turbo")
+        self.model_dropdown.pack(side="left", padx=10)
+
+        # Temperature slider
+        temp_label = ctk.CTkLabel(
+            settings_frame, text="Temperature:", font=default_font
+        )
+        temp_label.pack(side="left", padx=10)
+
+        self.temp_slider = ctk.CTkSlider(settings_frame, number_of_steps=100)
+        self.temp_slider.set(0.2)
+        self.temp_slider.pack(side="left", padx=10)
+
+    def generate_text(self):
+        self.save_to_db()
+        # just adding a new line to the text area here for now
+        self.text_area.insert(END, "\n")
+        prompt = self.prompt_entry.get()
+        model = self.model_dropdown.get()
+        temperature = self.temp_slider.get()
+
+        self.prompt_entry.delete(0, END)
+
+        def process_response(response):
+            self.full_response += response
+            
+            self.text_area.insert(END, response)
+            self.text_area.see(END)
 
 
-# Main window
-root = ctk.CTk()
-root.title("AI Text Generator")
-root.geometry("600x625")
+        def do_generate():
+            response = ask_gpt(prompt, model, temperature)
 
-default_font = ctk.CTkFont(family="Roboto", size=12)
-text_area_font = ctk.CTkFont(family="Roboto", size=14)
+            for chunk in response:
+                if chunk.choices[0].delta.content is not None:
+                    threading.Thread(
+                        target=process_response, args=(chunk.choices[0].delta.content,)
+                    ).start()
 
-# Input frame
-input_frame = ctk.CTkFrame(root)
-input_frame.pack(pady=20, padx=20, fill="x")
+        threading.Thread(target=do_generate).start()
 
-# Prompt input
-prompt_label = ctk.CTkLabel(input_frame, text="Enter Prompt:", font=default_font)
-prompt_label.pack(side="left", padx=10)
-prompt_entry = ctk.CTkEntry(input_frame, width=200)
-prompt_entry.pack(side="left", expand=True, fill="x")
+    def save_to_db(self):
+        if self.full_response:
+            add_message("assistant", self.full_response)
+            
+            self.full_response = ""
 
-# Generate button
-generate_button = ctk.CTkButton(
-    input_frame, text="Generate", command=on_generate_click, font=default_font
-)
-generate_button.pack(side="left", padx=10)
-
-# Text area for output
-text_area = scrolledtext.ScrolledText(root, wrap="word", font=text_area_font)
-text_area.pack(pady=10, padx=20, expand=True, fill="both")
-
-# Settings panel
-settings_panel = ctk.CTkFrame(root)
-settings_panel.pack(pady=10, padx=20, fill="x")
-
-# Model dropdown
-model_label = ctk.CTkLabel(settings_panel, text="Model:", font=default_font)
-model_label.pack(side="left", padx=10)
-model_dropdown = ctk.CTkComboBox(
-    settings_panel, values=["GPT 3.5 Turbo", "GPT 4", "GPT 4 Turbo"], font=default_font
-)
-model_dropdown.set("GPT 3.5 Turbo")
-model_dropdown.pack(side="left", padx=10)
-
-# Temperature slider
-temperature_label = ctk.CTkLabel(settings_panel, text="Temperature:", font=default_font)
-temperature_label.pack(side="left", padx=10)
-temperature_slider = ctk.CTkSlider(settings_panel, number_of_steps=100)
-temperature_slider.set(0.2)
-temperature_slider.pack(side="left", padx=10)
-
-# Run the application
 if __name__ == "__main__":
-    init_db()
-    root.mainloop()
+    app = TextGeneratorUI()
+    app.mainloop()
